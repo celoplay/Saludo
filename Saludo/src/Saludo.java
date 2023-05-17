b2b1a45140a1c1228e0a5768249849ca798bd255
maizaga <celoaizaga@gmail.com>
2023-05-17 13:05:05 +0200

    Version 2

--- Saludo/src/Saludo.java
+++ Saludo/src/Saludo.java
@@ -2,6 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Saludo {
     public static void main(String[] args) {
-        System.out.println("Hola, me llamo Marcelo Aizaga");
+
+        System.out.println("Hola, me llamo Marcelo Aizaga y me gusta el batido de fresa.");
+
     }
 }
